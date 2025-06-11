48e01f70e9887ec606b30599b4364c5232b81e4b
Anthony Pitman <42813672+AnthonyPitman@users.noreply.github.com>
2025-06-10 23:03:04 -0400

    add initial code for basic validation

---
+++ src/IpValidator/IpValidatorCli/Ipv4Validator.cs
@@ -0,0 +1,33 @@
+﻿namespace IpValidatorCli;
+
+public class Ipv4Validator
+{
+    public static string Validate(string input)
+    {
+        var split = input.Split('.');
+        if (split.Length != 4)
+        {
+            return $"Invalid format `{input}` should be in format `192.168.1.1`.";
+        }
+
+        foreach (var s in split)
+        {
+            if (!int.TryParse(s, out var octet))
+            {
+                return "Each octet inside the IP address should be integers.";
+            }
+
+            if (octet < 0)
+            {
+                return "Each octet must have a minimum value of 0.";
+            }
+
+            if (octet > 255)
+            {
+                return "Each octet must have a maximum value of 255";
+            }
+        }
+
+        return "";
+    }
+}
--- src/IpValidator/IpValidatorCli/Program.cs
+++ src/IpValidator/IpValidatorCli/Program.cs
@@ -1,9 +1,50 @@
 ﻿namespace IpValidatorCli;
 
-class Program
+internal static class Program
 {
-    static void Main(string[] args)
+    static int Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        if (args.Length == 0)
+        {
+            var exit = RunInteractiveMode();
+            return exit;
+        }
+
+        if (args.Length != 1)
+        {
+            Console.WriteLine("Usage - ipv4-validator.exe [<IPv4 Address>]");
+            return -1;
+        }
+
+        Console.WriteLine($"Input: {args.First()}");
+        var result = Validate(args.First());
+        return result;
+    }
+
+    static int RunInteractiveMode()
+    {
+        Console.Write("Enter IPv4 Address to be validated: ");
+        var input = Console.ReadLine() ?? "";
+        if (string.IsNullOrWhiteSpace(input.Trim()))
+        {
+            Console.WriteLine("Invalid. Input an Ipv4 address in the format such as 192.168.1.1");
+            return -1;
+        }
+
+        var result = Validate(input);
+        return result;
+    }
+
+    static int Validate(string input)
+    {
+        var result = Ipv4Validator.Validate(input);
+        if (result != "")
+        {
+            Console.WriteLine(result);
+            return -1;
+        }
+
+        Console.WriteLine($"The input `{input}` validated successfully.");
+        return 0;
     }
 }
